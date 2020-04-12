ad9d8e258871d0a179baf3cf53adad6b757a2092
Brad Janocha <bradjanocha@gmail.com>
2020-04-12 09:38:15 -0400

    Adding country input...

--- osm_snag.py
+++ osm_snag.py
@@ -3,8 +3,11 @@ import os, overpy
 
 api = overpy.Overpass()
 
+print("Enter the ISO code of the country for which you want data:")
+country = input()
+
 #uses the Overpass API's unique query language to 
-resultMA = api.query("""area["ISO3166-1"="MA"][admin_level=2];node["place"="city"](area);out;""")
+resultMA = api.query(f"""area["ISO3166-1"={country}][admin_level=2];node["place"="city"](area);out;""")
 resultDZ = api.query("""area["ISO3166-1"="DZ"][admin_level=2];node["place"="city"](area);out;""")
 
-print("Morocco has " + str(len(resultMA.nodes)) + " cities on OSM while Algeria has " + str(len(resultDZ.nodes)) + ".")
+print("The requested country has " + str(len(resultMA.nodes)) + " cities on OSM while Algeria has " + str(len(resultDZ.nodes)) + ".")
